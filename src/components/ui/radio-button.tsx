018029a63d881c07eba142d89d4211b1f6ade6f6
kimdw524 <kimdw524@naver.com>
2025-03-08 03:32:41 +0900

    feat: add RadioButton Component

---
+++ src/components/ui/radio-button.tsx
@@ -0,0 +1,64 @@
+import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
+
+import { Button } from './button';
+
+interface RadioButtonProps {
+  children: ReactNode;
+  defaultValue: string;
+  onChange: (value: string) => void;
+}
+
+const RadioButtonContext = createContext<{ value: string; onChange: RadioButtonProps['onChange'] } | undefined>(
+  undefined,
+);
+
+const RadioButton = ({ children, defaultValue, onChange }: RadioButtonProps) => {
+  const [value, setValue] = useState(defaultValue);
+
+  const handleChange = useCallback(
+    (value: string) => {
+      setValue(value);
+      onChange(value);
+    },
+    [onChange],
+  );
+
+  return (
+    <RadioButtonContext.Provider value={useMemo(() => ({ value, onChange: handleChange }), [value, handleChange])}>
+      <div>{children}</div>
+    </RadioButtonContext.Provider>
+  );
+};
+
+interface RadioButtonItemProps {
+  children: ReactNode;
+  value: string;
+}
+
+const RadioButtonItem = ({ children, value }: RadioButtonItemProps) => {
+  const radioButtonContext = useContext(RadioButtonContext);
+
+  if (!radioButtonContext) {
+    throw new Error('RadioButtonItem must be placed within a RadioButton.');
+  }
+
+  const handleClick = () => {
+    if (value === radioButtonContext.value) {
+      return;
+    }
+
+    radioButtonContext.onChange(value);
+  };
+
+  return (
+    <Button
+      variant={value === radioButtonContext.value ? 'secondary' : 'ghost'}
+      className="cursor-pointer"
+      onClick={handleClick}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export { RadioButton, RadioButtonItem };
