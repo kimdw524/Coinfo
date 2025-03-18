f0f2bb1fb460fa03049f389954bd2716099eaa32
kimdw524 <kimdw524@naver.com>
2025-03-19 03:44:42 +0900

    feat: home 페이지 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 
-import Navbar from '@/components/layout/Navbar';
 import ThemeProvider from '@/components/ThemeProvider';
 
 import './globals.css';
@@ -25,7 +24,6 @@ export default function RootLayout({
     <html lang="ko" suppressHydrationWarning>
       <body className={pretendard.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 'use client';
 
+import { StarIcon } from 'lucide-react';
+
+import Navbar from '@/components/layout/Navbar';
+import AssetWatchList from '@/components/market/AssetWatchList';
+import { currencies } from '@/constants/currencies';
+import useRealTimePrice from '@/hooks/useRealTimePrice';
+
 export default function Home() {
-  return <div></div>;
+  useRealTimePrice(currencies.map((currency) => currency.symbol));
+
+  return (
+    <>
+      <Navbar sticky />
+      <h1 className="text-md flex items-center gap-1 px-3 py-2">
+        <StarIcon className="text-amber-300" fill="currentColor" size={30} />
+        <span>관심 종목</span>
+      </h1>
+      <AssetWatchList />
+    </>
+  );
 }
